af113736dd6234aee37451f5180bfa1e8f0a01b3
TalalAlmutairi <Almutiri.Talal@hotmail.com>
2020-07-21 17:17:38 +0300

    Add project files.

---
+++ AdoSqlServerHelperVB/Examples.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="Examples.aspx.vb" Inherits="AdoSqlServerHelperVB.Examples" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+       <div>
+            <asp:GridView ID="GridView1" runat="server"></asp:GridView> <br />
+            <asp:Label ID="lbMsg" runat="server" Text="Label"></asp:Label><br />
+            <asp:Button ID="btnLoadData" runat="server" Text="Load Data" OnClick="btnLoadData_Click" />
+            <asp:Button ID="btnSqlWhere" runat="server" Text="Sql Where" OnClick="btnSqlWhere_Click" />
+            <asp:Button ID="btnExecuteScalar" runat="server" Text="Execute Scalar" OnClick="btnExecuteScalar_Click" />
+            <asp:Button ID="btnSP" runat="server" Text="Stored Procedure" OnClick="btnSP_Click" />
+             <asp:Button ID="btnExecuteTransaction" runat="server" Text="Execute Transaction" OnClick="btnExecuteTransaction_Click" />
+            <asp:Button ID="btnReturnDS" runat="server" Text="DataSet" OnClick="btnReturnDS_Click" />
+            <asp:GridView ID="GridViewEmp" runat="server"></asp:GridView> <br />
+            <br />
+            <asp:GridView ID="GridViewCountry" runat="server"></asp:GridView> <br />
+            For Insert, update, and delete <br />
+            <input id="txtEmpID" type="text" runat="server" placeholder="Employee ID" />
+            <input id="txtFName" type="text" runat="server" placeholder="First Name" />
+            <input id="txtLName" type="text" runat="server" placeholder="Last Name" />
+            <input id="txtAge" type="number" min="18" max="50" step="1" runat="server" placeholder="Age" />
+            <select id="ddlCouontries" runat="server">
+                <option value="1">Saudi Arabia</option>
+                <option value="2">Kuwait</option>
+                 <option value="3">United Arab Emirates</option>
+            </select>
+            <br />
+            <asp:Button ID="btnInsert" runat="server" Text="Insert" OnClick="btnInsert_Click" />
+            <asp:Button ID="btnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click" /> 
+            <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" /> <br />
+           
+        </div>
+    </form>
+</body>
+</html>
---
+++ AdoSqlServerHelperVB/Examples.aspx.vb
@@ -0,0 +1,133 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Examples
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    ' Getting all employees data
+    Protected Sub btnLoadData_Click(sender As Object, e As EventArgs) Handles btnLoadData.Click
+        Dim sql As String = "SELECT * FROM Employees"
+        GridView1.DataSource = SqlHelper.ExecuteQuery(sql, CommandType.Text, Nothing)
+        GridView1.DataBind()
+    End Sub
+
+    ' Selecting only the employee with id=2
+    Protected Sub btnSqlWhere_Click(sender As Object, e As EventArgs) Handles btnSqlWhere.Click
+        Dim sql As String = "SELECT * FROM Employees WHERE EmpID=@ID"
+
+        'SqlParameter uses for security to prevent SQL injection
+        '@ID in the SQL above must match the parameter in SqlParameter
+        Dim parametersList As SqlParameter() = New SqlParameter() {
+            New SqlParameter("@ID", "1")
+        }
+        GridView1.DataSource = SqlHelper.ExecuteQuery(sql, CommandType.Text, parametersList)
+        GridView1.DataBind()
+    End Sub
+
+    ' Getting the maximum salary of all employees as only one value
+    Protected Sub btnExecuteScalar_Click(sender As Object, e As EventArgs) Handles btnExecuteScalar.Click
+        Dim sql As String = "SELECT MAX(Age) FROM Employees"
+        lbMsg.Text = SqlHelper.ExecuteScalar(sql, CommandType.Text, Nothing)
+    End Sub
+
+    ' Execute stored procedure
+    Protected Sub btnSP_Click(sender As Object, e As EventArgs) Handles btnSP.Click
+        GridView1.DataSource = SqlHelper.ExecuteQuery("GetAllEmployees", CommandType.StoredProcedure, Nothing)
+        GridView1.DataBind()
+    End Sub
+
+    Protected Sub btnInsert_Click(sender As Object, e As EventArgs) Handles btnInsert.Click
+        Dim sql As String = "INSERT INTO Employees VALUES(@FName,@LName,@Age,@CountryID)"
+        Dim parametersList As SqlParameter() = New SqlParameter() {
+            New SqlParameter("@FName", txtFName.Value),
+            New SqlParameter("@LName", txtLName.Value),
+            New SqlParameter("@Age", txtAge.Value),
+            New SqlParameter("@CountryID", ddlCouontries.Value)
+        }
+
+        If SqlHelper.ExecuteNonQuery(sql, CommandType.Text, parametersList) Then
+            lbMsg.Text = "Inserted successfully"
+        Else
+            lbMsg.Text = "Error"
+        End If
+    End Sub
+
+    Protected Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        Dim sql As String = "UPDATE Employees SET FirstName=@FName,LastName=@LName,Age=@Age,CountryID=@CountryID WHERE EmpID =@ID"
+        Dim parametersList As SqlParameter() = New SqlParameter() {
+            New SqlParameter("@ID", txtEmpID.Value),
+            New SqlParameter("@FName", txtFName.Value),
+            New SqlParameter("@LName", txtLName.Value),
+            New SqlParameter("@Age", txtAge.Value),
+            New SqlParameter("@CountryID", ddlCouontries.Value)
+        }
+
+        If SqlHelper.ExecuteNonQuery(sql, CommandType.Text, parametersList) Then
+            lbMsg.Text = "Updated successfully"
+        Else
+            lbMsg.Text = "Error"
+        End If
+    End Sub
+
+    Protected Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Dim sql As String = "DELETE FROM Employees WHERE EmpID =@ID"
+        Dim parametersList As SqlParameter() = New SqlParameter() {
+            New SqlParameter("@ID", txtEmpID.Value)
+        }
+
+        If SqlHelper.ExecuteNonQuery(sql, CommandType.Text, parametersList) Then
+            lbMsg.Text = "Deleted successfully"
+        Else
+            lbMsg.Text = "Error"
+        End If
+    End Sub
+
+    'Execute two SQL statements Insert and update, which all SQL statements in a single transaction, rolling back if an error has occurred
+    Protected Sub btnExecuteTransaction_Click(sender As Object, e As EventArgs) Handles btnExecuteTransaction.Click
+        Dim listOfSQLs As ArrayList = New ArrayList()
+        Dim listOfParamerters As List(Of SqlParameter()) = New List(Of SqlParameter())()
+
+        Dim sql1 As String = "INSERT INTO Employees VALUES(@FName,@LName,@Age,@CountryID)"
+        Dim parameters1 As SqlParameter() = New SqlParameter() {
+            New SqlParameter("@FName", "Test F Name"),
+            New SqlParameter("@LName", "Test L Name"),
+            New SqlParameter("@Age", 25),
+            New SqlParameter("@CountryID", 1)
+            }
+        listOfSQLs.Add(sql1)
+        listOfParamerters.Add(parameters1)
+
+        Dim sql2 As String = "UPDATE Employees SET FirstName=@FName,LastName=@LName,Age=@Age,CountryID=@CountryID WHERE EmpID =@ID"
+        Dim parameters2 As SqlParameter() = New SqlParameter() {
+            New SqlParameter("@ID", 4),
+            New SqlParameter("@FName", "New F Name"),
+            New SqlParameter("@LName", "New L Name"),
+            New SqlParameter("@Age", 30),
+            New SqlParameter("@CountryID", 2)
+        }
+        listOfSQLs.Add(sql2)
+        listOfParamerters.Add(parameters2)
+
+        If SqlHelper.ExecuteTransaction(listOfSQLs, listOfParamerters) Then
+            lbMsg.Text = "All SQL statements executed successfully"
+        Else
+            lbMsg.Text = "Error"
+        End If
+    End Sub
+
+    'Execute two select queries, and returns employees and country tables
+    Protected Sub btnReturnDS_Click(sender As Object, e As EventArgs) Handles btnReturnDS.Click
+        ' You can also use a stored procedure instead  of two queries in one string variable
+        Dim sql As String = "SELECT * FROM Employees; SELECT * FROM Country"
+        Dim ds As DataSet = SqlHelper.ExecuteQueryDS(sql, CommandType.Text, Nothing)
+
+        GridViewEmp.DataSource = ds.Tables(0)
+        GridViewEmp.DataBind()
+
+        GridViewCountry.DataSource = ds.Tables(1)
+        GridViewCountry.DataBind()
+    End Sub
+End Class
